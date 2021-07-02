064a80eca7203acd5b504aac84b7aeee81432776
mjtechguy <mikejohnsonit@gmail.com>
2021-07-02 17:15:47 +0000

    add provider.tf and update gitignore

---
+++ terraform/do_default/provider.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "replace-me-digitalocean-provider-source"
+      version = "replace-me-digitalocean-provider-version"
+    }
+    cloudflare = {
+      source = "replace-me-cloudflare-provider-source"
+    }
+  }
+  backend "replace-me-terraform-backend-type" {
+    bucket = "replace-me-aws-bucket-name"
+    access_key = "replace-me-aws-access-key"
+    secret_key = "replace-me-aws-secret-key"
+    region = "replace-me-aws-region"
+    key = "replace-me-terraform-backend-key"
+  }
+  required_version = ">= replace-me-terraform-required-version"
+}
+
+provider "digitalocean" {
+  token = var.digitalocean_token
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
